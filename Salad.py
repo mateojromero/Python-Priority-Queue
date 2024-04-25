9af01d193f5c7c55a5f18322905b978ca6776aa3
mateojromero <168132449+mateojromero@users.noreply.github.com>
2024-04-25 15:14:21 -0700

    Create Salad.py

---
+++ Salad.py
@@ -0,0 +1,17 @@
+
+class Salad:
+    def __init__(self, size):
+        self.size = size
+        self.price = 0.0
+    
+    def set_price(self, price):
+        self.price = price
+
+    def get_price(self):
+        return self.price
+    
+    def set_size(self, size):
+        self.size = size
+
+    def get_size(self):
+        return self.size
